bc4031fbfd429764bdfafc76a63bd78dddac6b6f
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2022-06-24 14:00:21 +0200

    fix link to documentation

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -31,7 +31,7 @@ const LinkNav: React.FC = () => {
 			tooltip: 'Twitter',
 		},
 		{
-			link: 'http://localhost:8080/get-started/overview/',
+			link: 'https://docs.huff.sh/',
 			icon: 'book',
 			tooltip: 'Documentation & Specification',
 		},
