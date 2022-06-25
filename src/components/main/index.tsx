89c64dbfc082605e7d117bed5a0c5d1604cc8914
Andreas Bigger <abigger87@gmail.com>
2022-06-25 15:56:07 -0400

    invalid url

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -14,7 +14,7 @@ export const Main: React.FC = () => {
 					<span className='mono-font'>
 						Huff
 						<p>
-							<Badge bg='secondary'>curl -L https://get.huff.sh | bash</Badge>
+							<Badge bg='secondary'>curl -L get.huff.sh | bash</Badge>
 							<br />A low level assembly language for the EVM.
 						</p>
 					</span>
